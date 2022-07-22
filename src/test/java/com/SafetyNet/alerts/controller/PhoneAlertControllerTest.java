6c86a7eeef58fe24d8ce2ad61f6215cc32f5c0af
oksanaleroy <oksanaleroy@localhost>
2022-07-22 23:46:45 +0200

    Add phoneAlertControllerTest

---
+++ src/test/java/com/SafetyNet/alerts/controller/PhoneAlertControllerTest.java
@@ -0,0 +1,54 @@
+package com.SafetyNet.alerts.controller;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.SafetyNet.alerts.service.PhoneAlertService;
+
+public class PhoneAlertControllerTest {
+
+	@Mock
+	private PhoneAlertService phoneAlertService;
+
+	@InjectMocks
+	private PhoneAlertController phoneAlertController;
+	private MockMvc mockMvc; 
+	
+	Set<String> phones = new HashSet<>();
+
+	@BeforeEach
+	void setup() {
+		phones.add("841-874-6512");
+		phones.add("849-374-7532");
+		MockitoAnnotations.openMocks(this);  		
+		this.mockMvc = MockMvcBuilders.standaloneSetup(phoneAlertController)
+			.build();
+
+	}
+	
+	@Test
+	public void shouldphoneAlertControllerTest() throws Exception {
+		when(phoneAlertService.listOfPhonePerNumFireStation("2"))
+			.thenReturn(phones);
+
+		mockMvc.perform( 							
+				MockMvcRequestBuilders.get("/phoneAlert?firestation=2")	
+				.accept(MediaType.APPLICATION_JSON))
+			.andDo(print())
+			.andExpect(status().isOk());
+	}
+}
